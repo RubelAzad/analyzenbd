37816357e4a49264b1c826b2623fb5c0492c82f5
RubelAzad <rubelazad123@gmail.com>
2024-03-19 11:26:14 +0600

    unit test function done and soft delete design improve

--- resources/views/users/deleted.blade.php
+++ resources/views/users/deleted.blade.php
@@ -1,4 +1,3 @@
-<!-- deleted.blade.php -->
 @extends('layouts.app')
 
 @section('content')
@@ -11,34 +10,36 @@
             </div>
         </div>
         <h2>Soft-Deleted Users</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($deletedUsers as $user)
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
                     <tr>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>
-                            <form action="{{ route('restore', $user->id) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('PUT')
-                                <button type="submit" class="btn btn-success">Restore</button>
-                            </form>
-                            <form action="{{ route('destroy-permanently', $user->id) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Permanently Delete</button>
-                            </form>
-                        </td>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($deletedUsers as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <form action="{{ route('restore', $user->id) }}" method="POST" style="display: inline;">
+                                    @csrf
+                                    @method('PUT')
+                                    <button type="submit" class="btn btn-success btn-sm">Restore</button>
+                                </form>
+                                <form action="{{ route('destroy-permanently', $user->id) }}" method="POST" style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Permanently Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
