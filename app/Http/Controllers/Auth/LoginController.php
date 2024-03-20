52ec7776dea978e6dfa2f8e1f09057c8f75cba7f
RubelAzad <rubelazad123@gmail.com>
2024-03-20 13:29:26 +0600

    home route path change

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/users';
+    protected $redirectTo = '/customuser';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/customuser';
 
     /**
      * Create a new controller instance.
